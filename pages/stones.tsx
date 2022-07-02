98873b6b199562ad8bb70ef6a476eabfb15e254f
Yuri <yurinakanishi33@gmail.com>
2022-07-02 22:37:37 +0900

    c

--- pages/stones.tsx
+++ pages/stones.tsx
@@ -1,18 +1,19 @@
-// import '../src/components/styles/global.css'
+import { BottomNav, Footer } from '../src/components'
+import '../src/components/styles/global.css'
 // import { Footer, BottomNav, Countries, StoneFirstSlider, StoneVideoWindow, StoneImgWindow, StoneTopic } from '../src/components/index'
 
-// function Stone() {
-//   return (
-//     <div className='index'>
-//       <StoneFirstSlider />
-//       <StoneTopic />
-//       <StoneVideoWindow />
-//       <StoneImgWindow />
-//       <Countries />
-//       <Footer />
-//       <BottomNav />
-//     </div>
-//   )
-// }
+function Stone() {
+  return (
+    <div className='index'>
+      {/* <StoneFirstSlider />
+      <StoneTopic />
+      <StoneVideoWindow />
+      <StoneImgWindow />
+      <Countries /> */}
+      <Footer />
+      <BottomNav />
+    </div>
+  )
+}
 
-// export default Stone
+export default Stone
